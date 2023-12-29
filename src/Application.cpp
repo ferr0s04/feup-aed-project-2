f27763439b7cf6534bf893165eea66ef2fc7263a
Isabel <isabel.oliveira.pt@gmail.com>
2023-12-29 15:16:39 +0000

    fixed csv path

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,7 +3,7 @@
 Application::Application() {}
 
 void Application::run() {
-    DataHandler CSV_Data("resources/airlines.csv", "resources/airports.csv", "resources/flights.csv");
+    DataHandler CSV_Data("../resources/airlines.csv", "../resources/airports.csv", "../resources/flights.csv");
     Airline::setAirlines(CSV_Data.getParsedAirlines());
     Airport::setAirports(CSV_Data.getParsedAirports());
     Flight::setFlights(CSV_Data.getParsedFlights());
